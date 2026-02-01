6f63b5fadc1d80a1ae0cbcee8b7dfdb301fa7d31
Stefan <stefansnikolov18@abv.bg>
2026-02-01 21:58:32 +0200

    add Sha256Hashing

---
+++ src/password/vault/server/util/Sha256Hashing.java
@@ -0,0 +1,28 @@
+package password.vault.server.util;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Sha256Hashing {
+
+    public static String hashing(String password) {
+        String hashedGivenPassword;
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+
+            return bytesToHex(hashBytes);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("SHA-256 algorithm not found", e);
+        }
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+}
